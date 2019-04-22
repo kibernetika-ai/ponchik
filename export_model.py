3c7a2138af97850e2f774474dee0060df82c18ca
Vadim <monstarnn@gmail.com>
2019-04-22 19:29:36 +0300

    export model script

---
+++ export_model.py
@@ -0,0 +1,37 @@
+import argparse
+
+from svod_rcgn.mlboard import mlboard, catalog_ref
+from svod_rcgn.recognize.args import add_common_args
+from svod_rcgn.tools.print import print_fun
+
+
+def main():
+
+    if not mlboard:
+        print_fun("Skipped: no mlboard detected")
+        return
+
+    parser = argparse.ArgumentParser()
+    add_common_args(parser)
+    parser.add_argument(
+        '--model_name',
+        help='Exported model name.',
+        type=str,
+        default='svod-faces',
+    )
+    parser.add_argument(
+        '--model_version',
+        help='Exported model version.',
+        type=str,
+        default='1.0.0',
+    )
+    args = parser.parse_args()
+
+    print_fun('Uploading model...')
+    mlboard.model_upload(args.model_name, args.model_version, args.classifiers_dir)
+    mlboard.update_task_info({'model_reference': catalog_ref(args.model_name, 'mlmodel', args.model_version)})
+    print_fun("New model uploaded as '%s', version '%s'." % (args.model_name, args.model_version))
+
+
+if __name__ == '__main__':
+    main()
---
+++ svod_rcgn/mlboard/__init__.py
@@ -0,0 +1,26 @@
+import os
+
+from svod_rcgn.tools.print import print_fun
+
+try:
+    from mlboardclient.api import client
+except ImportError:
+    client = None
+
+
+mlboard = None
+
+if client:
+    mlboard = client.Client()
+    try:
+        mlboard.apps.get()
+    except Exception:
+        mlboard = None
+        print_fun('Do not use mlboard.')
+    else:
+        print_fun('Use mlboard parameters logging.')
+
+
+def catalog_ref(name, ctype, version):
+    return '#/{}/catalog/{}/{}/versions/{}'. \
+        format(os.environ.get('WORKSPACE_NAME'), ctype, name, version)
