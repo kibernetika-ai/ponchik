df4dfe48523b1f5f2ec91569c00e2aeb2f9a8196
Vadim <monstarnn@gmail.com>
2019-04-23 15:37:26 +0300

    store and log full aligned info

--- svod_rcgn/dataset/aligner.py
+++ svod_rcgn/dataset/aligner.py
@@ -125,6 +125,7 @@ class Aligner:
             "min_face_size": self.min_face_size,
             "image_size": self.image_size,
             "margin": self.margin,
+            "bg_remove_path": self.bg_remove_path,
         }
 
         align_data = {}
@@ -170,104 +171,115 @@ class Aligner:
 
         self.min_face_area = self.min_face_size ** 2
 
-        with open(bounding_boxes_filename, "w") as text_file:
-            nrof_images_total = 0
-            nrof_images_cached = 0
-            nrof_successfully_aligned = 0
-            for cls in loaded_dataset:
-                output_class_dir = os.path.join(aligned_dir, cls.name)
-                output_class_dir_created = False
-                aligned_class_images = []
-                if cls.name in align_data:
-                    align_data_class = align_data[cls.name]
-                else:
-                    align_data_class = {}
-                for image_path in cls.image_paths:
-                    nrof_images_total += 1
-                    filename = os.path.splitext(os.path.split(image_path)[1])[0]
-                    output_filename = os.path.join(output_class_dir, filename + '.png')
-                    if not os.path.exists(output_filename):
-                        try:
-                            img = cv2.imread(image_path, cv2.IMREAD_COLOR).astype(np.float32)
-                        except Exception as e:
-                            error_message = '{}: {}'.format(image_path, e)
-                            print_fun('ERROR: %s' % error_message)
-                            continue
+        bounding_boxes_contents = ""
 
-                        img_hash = hashlib.sha1(img.tostring()).hexdigest()
-                        if image_path in align_data_class:
-                            if 'hash' in align_data_class[image_path]:
-                                if align_data_class[image_path]['hash'] == img_hash:
-                                    all_aligned_exists = True
+        nrof_images_total = 0
+        nrof_images_cached = 0
+        nrof_successfully_aligned = 0
+        for cls in loaded_dataset:
+            output_class_dir = os.path.join(aligned_dir, cls.name)
+            output_class_dir_created = False
+            aligned_class_images = []
+            if cls.name in align_data:
+                align_data_class = align_data[cls.name]
+            else:
+                align_data_class = {}
+            for image_path in cls.image_paths:
+                nrof_images_total += 1
+                filename = os.path.splitext(os.path.split(image_path)[1])[0]
+                output_filename = os.path.join(output_class_dir, filename + '.png')
+                if not os.path.exists(output_filename):
+                    try:
+                        img = cv2.imread(image_path, cv2.IMREAD_COLOR).astype(np.float32)
+                    except Exception as e:
+                        error_message = '{}: {}'.format(image_path, e)
+                        print_fun('ERROR: %s' % error_message)
+                        continue
+
+                    img_hash = hashlib.sha1(img.tostring()).hexdigest()
+                    if image_path in align_data_class:
+                        if 'hash' in align_data_class[image_path]:
+                            if align_data_class[image_path]['hash'] == img_hash:
+                                all_aligned_exists = True
+                                if 'aligned' in align_data_class[image_path]:
+                                    for a in align_data_class[image_path]['aligned']:
+                                        if not os.path.isfile(a):
+                                            all_aligned_exists = False
+                                            break
+                                if all_aligned_exists:
                                     if 'aligned' in align_data_class[image_path]:
-                                        for a in align_data_class[image_path]['aligned']:
-                                            if not os.path.isfile(a):
-                                                all_aligned_exists = False
-                                                break
-                                    if all_aligned_exists:
-                                        # print_fun("%s - cached" % image_path)
-                                        # self._clear_absent_aligned(
-                                        #     output_class_dir, align_data_class[image_path]['aligned'])
-                                        if 'aligned' in align_data_class[image_path]:
-                                            aligned_class_images.extend(align_data_class[image_path]['aligned'])
-                                        nrof_images_cached += 1
-                                        continue
-                        align_data_class[image_path] = {'hash': hashlib.sha1(img.tostring()).hexdigest()}
-                        print_fun(image_path)
+                                        aligned_class_images.extend(
+                                            list(align_data_class[image_path]['aligned'].keys()))
+                                        for _, a in enumerate(align_data_class[image_path]['aligned']):
+                                            b = align_data_class[image_path]['aligned'][a]
+                                            bounding_boxes_contents += \
+                                                '%s %d %d %d %d cached\n' % (a, b[0], b[1], b[2], b[3])
+                                    else:
+                                        bounding_boxes_contents += \
+                                            '%s ERROR no aligned cached\n' % image_path
 
-                        if len(img.shape) <= 2:
-                            print_fun('WARNING: Unable to align "%s", shape %s' % (image_path, img.shape))
-                            text_file.write('%s\n' % output_filename)
-                            continue
+                                    nrof_images_cached += 1
+                                    continue
 
-                        if bg_rm_drv is not None:
-                            img = bg_rm_drv.apply_mask(img)
+                    align_data_class[image_path] = {'hash': hashlib.sha1(img.tostring()).hexdigest()}
+                    print_fun(image_path)
 
-                        bounding_boxes = None
-                        if bg_rm_drv is not None:
-                            img_masked = bg_rm_drv.apply_mask(img)
-                            bounding_boxes = self._get_boxes(image_path, img_masked)
-                            if bounding_boxes is None:
-                                print_fun('WARNING: no faces on image with removed bg, try without bg removing')
+                    if len(img.shape) <= 2:
+                        print_fun('WARNING: Unable to align "%s", shape %s' % (image_path, img.shape))
+                        bounding_boxes_contents += '%s ERROR invalid shape\n' % image_path
+                        continue
 
-                        if bounding_boxes is None or bg_rm_drv is not None:
-                            bounding_boxes = self._get_boxes(image_path, img)
+                    if bg_rm_drv is not None:
+                        img = bg_rm_drv.apply_mask(img)
 
+                    bounding_boxes = None
+                    if bg_rm_drv is not None:
+                        img_masked = bg_rm_drv.apply_mask(img)
+                        bounding_boxes = self._get_boxes(image_path, img_masked)
                         if bounding_boxes is None:
-                            text_file.write('%s\n' % output_filename)
-                            continue
+                            print_fun('WARNING: no faces on image with removed bg, try without bg removing')
 
-                        imgs = images.get_images(
-                            img,
-                            bounding_boxes,
-                            face_crop_size=self.image_size,
-                            face_crop_margin=self.margin,
-                            do_prewhiten=False,
-                        )
-                        align_data_class[image_path]['aligned'] = []
-                        for i, cropped in enumerate(imgs):
-                            nrof_successfully_aligned += 1
-                            bb = bounding_boxes[i]
-                            filename_base, file_extension = os.path.splitext(output_filename)
-                            output_filename_n = "{}_{}{}".format(filename_base, i, file_extension)
-                            text_file.write('%s %d %d %d %d\n' % (output_filename_n, bb[0], bb[1], bb[2], bb[3]))
-                            if not output_class_dir_created:
-                                output_class_dir_created = True
-                                if not os.path.exists(output_class_dir):
-                                    os.makedirs(output_class_dir)
-                            cv2.imwrite(output_filename_n, cropped)
-                            align_data_class[image_path]['aligned'].append(output_filename_n)
+                    if bounding_boxes is None or bg_rm_drv is not None:
+                        bounding_boxes = self._get_boxes(image_path, img)
 
-                        aligned_class_images.extend(align_data_class[image_path]['aligned'])
+                    if bounding_boxes is None:
+                        bounding_boxes_contents += '%s ERROR no faces detected\n' % image_path
+                        continue
 
-                if not self.complementary_align:
-                    if os.path.isdir(output_class_dir):
-                        for f in os.listdir(output_class_dir):
-                            fp = os.path.join(output_class_dir, f)
-                            if os.path.isfile(fp) and fp not in aligned_class_images:
-                                os.remove(fp)
+                    imgs = images.get_images(
+                        img,
+                        bounding_boxes,
+                        face_crop_size=self.image_size,
+                        face_crop_margin=self.margin,
+                        do_prewhiten=False,
+                    )
+                    align_data_class[image_path]['aligned'] = {}
+                    for i, cropped in enumerate(imgs):
+                        nrof_successfully_aligned += 1
+                        bb = bounding_boxes[i]
+                        filename_base, file_extension = os.path.splitext(output_filename)
+                        output_filename_n = "{}_{}{}".format(filename_base, i, file_extension)
+                        bounding_boxes_contents += '%s %d %d %d %d\n' % (output_filename_n, bb[0], bb[1], bb[2], bb[3])
+                        if not output_class_dir_created:
+                            output_class_dir_created = True
+                            if not os.path.exists(output_class_dir):
+                                os.makedirs(output_class_dir)
+                        cv2.imwrite(output_filename_n, cropped)
+                        align_data_class[image_path]['aligned'][output_filename_n] = (bb[0], bb[1], bb[2], bb[3])
 
-                align_data[cls.name] = align_data_class
+                    aligned_class_images.extend(list(align_data_class[image_path]['aligned'].keys()))
+
+            if not self.complementary_align:
+                if os.path.isdir(output_class_dir):
+                    for f in os.listdir(output_class_dir):
+                        fp = os.path.join(output_class_dir, f)
+                        if os.path.isfile(fp) and fp not in aligned_class_images:
+                            os.remove(fp)
+
+            align_data[cls.name] = align_data_class
+
+        with open(bounding_boxes_filename, "w") as text_file:
+            text_file.write(bounding_boxes_contents)
 
         with open(align_filename, 'wb') as align_file:
             pickle.dump((align_data_args, align_data), align_file, protocol=2)
