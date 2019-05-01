0e7f93b0758a0151ba25e5792fc7576fde3812b8
Alexander Gunin <guninalexander@gmail.com>
2019-04-30 17:09:55 -0700

    add real scenes

--- svod_rcgn/recognize/video.py
+++ svod_rcgn/recognize/video.py
@@ -64,7 +64,7 @@ class Video:
             import pyrealsense2 as rs
             self.pipeline = rs.pipeline()
             config = rs.config()
-            config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 10)
+            config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
             self.pipeline.start(config)
         else:
             self.vs = cv2.VideoCapture(self.video_source)
