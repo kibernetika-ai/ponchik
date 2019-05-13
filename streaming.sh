43a7455106cfe519c6196bfc004954e5bc9e36a6
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2019-05-13 16:39:20 +0400

    More advanced streaming.sh

--- streaming.sh
+++ streaming.sh
@@ -1,35 +1,90 @@
-rtmp_url=$1
-clf_path=$2
-inference_fps=$3
-model_path=$4
+rtmp_url=""
+clf_path=data/classifiers
+inference_fps=5
+model_path=models/facenet_pretrained_openvino_cpu/facenet.xml
 backend=livego # livego / rtmp-mux
+OUTPUT_TYPE="rtmp"
+INPUT="server"
 
-if [ -z "$rtmp_url" ];
+usage="""
+Usage:
+  $0 <options>
+
+Options:
+  
+  --output-type <rtmp/display> Output type: rtmp/display/'', default $OUTPUT_TYPE.
+  --classifiers <dir> Classifiers directory, default $clf_path.
+  --inference-fps <int> Inference FPS, default $inference_fps.
+  --model-path <path> Facenet model path, default $model_path
+  --output-rtmp <rtmp-url> Output RTMP URL stream address. Optional.
+  --input <type> Input type. Can be one of 'camera', 'realsense', 'server' 
+                 or any opencv-compatible URL (rtmp/rtsp/filepath etc.). Default $INPUT.
+
+
+In case of --output-type rtmp use ffmpeg to stream to this serving, e.g:
+ 
+  ffmpeg -re -i <video/source> -vcodec libx264 -acodec aac -f flv rtmp://localhost/live
+"""
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+  case $key in
+    --output-type)
+    OUTPUT_TYPE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    --classifiers)
+    clf_path="$2"
+    shift; shift
+    ;;
+    --inference-fps)
+    inference_fps="$2"
+    shift; shift
+    ;;
+    --model-path)
+    model_path="$2"
+    shift; shift
+    ;;
+    --output-rtmp)
+    rtmp_url="$2"
+    shift; shift;
+    ;;
+    --input)
+    INPUT="$2"
+    shift; shift
+    ;;
+    *)
+    echo "Unknown option $key."
+    echo "$usage"
+    exit 1
+    shift
+  esac
+done
+
+#echo "INPUT=$INPUT"
+#echo "OUTPUT_TYPE=$OUTPUT_TYPE"
+#echo "clf_path=$clf_path"
+#echo "inference_fps=$inference_fps"
+#echo "model_path=$model_path"
+
+if [ "$OUTPUT_TYPE" == "rtmp" ];
 then
-  echo "Usage: $0 <output_rtmp_url> [classifiers-dir] [inference_fps; default=2] [facenet-model-path; default=models/facenet_pretrained_openvino_cpu/facenet.xml]"
-  echo
-  echo "Then, use ffmpeg to stream to this serving, e.g:"
-  echo 
-  echo "  ffmpeg -re -i <video/source> -vcodec libx264 -acodec aac -f flv rtmp://localhost/live"
-  echo 
-  exit 1
+  if [ -z "$rtmp_url" ];
+  then
+    echo "--output-rtmp required in case of providing --output-type rtmp"
+    echo
+    echo "$usage"
+    exit 1
+  fi
 fi
 
-if [ -z "$clf_path" ];
+output_arg="--output-type $OUTPUT_TYPE"
+if [ -z "$OUTPUT_TYPE" ];
 then
-  clf_path=data/classifiers
+  output_arg=""
 fi
 
-if [ -z "$inference_fps" ];
-then
-  inference_fps=2
-fi
+kstreaming --driver openvino --model-path $model_path --hooks serving_hook.py -o classifiers_dir=$clf_path -o need_table=false -o timing=false -o output_type=image --input $INPUT $output_arg --output-rtmp "$rtmp_url" --initial-stream live --input-name input --output-name output --rtmp-backend $backend -o enable_log=true -o inference_fps=$inference_fps
 
-if [ -z "$model_path" ];
-then
-  model_path=models/facenet_pretrained_openvino_cpu/facenet.xml
-fi
-
-kstreaming --driver openvino --model-path $model_path --hooks serving_hook.py -o classifiers_dir=$clf_path -o need_table=false -o timing=false -o output_type=image --input server --output-type rtmp --output-rtmp $rtmp_url --initial-stream live --input-name input --output-name output --rtmp-backend $backend -o enable_log=true -o inference_fps=$inference_fps
-
-#kstreaming --driver openvino --model-path ~/projects/kiber-facenet/src/train-new/facenet.xml --hooks serving_hook.py -o classifiers_dir=clf -o need_table=false -o timing=false -o output_type=image --input server --output-type display --initial-stream live --input-name input --output-name output --rtmp-backend livego -o enable_log=true
