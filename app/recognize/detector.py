Add option to use fixed normalization

app/recognize/detector.py
@@ -39,6 +39,11 @@ def add_detector_args(parser):
         help='Threshold for detecting faces',
     )
     parser.add_argument(
+        '--fixed_normalization',
+        help='Use fixed image normalization during training.',
+        action='store_true',
+    )
+    parser.add_argument(
         '--debug',
         help='Full debug output for each detected face.',
         action='store_true',
@@ -94,6 +99,7 @@ def detector_args(args):
         process_not_detected=args.process_not_detected,
         account_head_pose=not args.head_pose_not_account,
         multi_detect=multi_detect,
+        fixed_normalization=args.fixed_normalization,
     )
 
 
@@ -141,6 +147,7 @@ class Detector(object):
             process_not_detected=False,
             account_head_pose=True,
             multi_detect=None,
+            fixed_normalization=False,
     ):
         self._initialized = False
         self.face_driver: driver.ServingDriver = face_driver
@@ -155,6 +162,7 @@ class Detector(object):
         self.account_head_pose = account_head_pose
 
         self.use_classifiers = False
+        self.normalization = images.NORMALIZE_FIXED if fixed_normalization else images.NORMALIZE_IMAGE
         self.classifiers = DetectorClassifiers()
         self.threshold = threshold
         self.multi_detect = multi_detect
@@ -459,7 +467,7 @@ class Detector(object):
         if boxes is None or len(boxes) == 0:
             return []
 
-        imgs = np.stack(images.get_images(bgr_frame, boxes, 60, 0, do_prewhiten=False))
+        imgs = np.stack(images.get_images(bgr_frame, boxes, 60, 0, normalize=None))
         outputs = self.head_pose_driver.predict({'data': imgs.transpose([0, 3, 1, 2])})
 
         yaw = outputs[self.head_pose_yaw].reshape([-1])
@@ -530,7 +538,7 @@ class Detector(object):
         else:
             bboxes = self.detect_faces(frame, self.threshold, self.multi_detect)
             poses = self.wrong_pose_indices(frame, bboxes)
-            imgs = images.get_images(frame, bboxes)
+            imgs = images.get_images(frame, bboxes, normalize=self.normalization)
 
         skips = self.wrong_pose_skips(poses)
         # skips, poses = self.skip_wrong_pose_indices(frame, bboxes)

app/tools/dataset.py
@@ -75,7 +75,7 @@ def get_meta(dataset):
     return meta
 
 
-def load_data(image_paths, image_size, do_prewhiten=True):
+def load_data(image_paths, image_size, fixed_normalization=False):
     nrof_samples = len(image_paths)
     imgs = np.zeros((nrof_samples, image_size, image_size, 3))
     for i in range(nrof_samples):
@@ -86,7 +86,9 @@ def load_data(image_paths, image_size, do_prewhiten=True):
         if len(img.shape) >= 3 and img.shape[2] > 3:
             # RGBA, convert to RGB
             img = np.array(Image.fromarray(img).convert('RGB'))
-        if do_prewhiten:
+        if fixed_normalization:
+            img = images.fixed_normalize(img)
+        else:
             img = images.prewhiten(img)
         imgs[i, :, :, :] = img
     return imgs

app/tools/images.py
@@ -2,7 +2,11 @@ import cv2
 import numpy as np
 
 
-def get_images(image, bounding_boxes, face_crop_size=160, face_crop_margin=32, do_prewhiten=True):
+NORMALIZE_FIXED = 1
+NORMALIZE_IMAGE = 2
+
+
+def get_images(image, bounding_boxes, face_crop_size=160, face_crop_margin=32, normalize=None):
     images = []
 
     nrof_faces = bounding_boxes.shape[0]
@@ -25,8 +29,10 @@ def get_images(image, bounding_boxes, face_crop_size=160, face_crop_margin=32, d
             bb[3] = np.minimum(det[3] + face_crop_margin / 2, img_size[0])
             cropped = image[bb[1]:bb[3], bb[0]:bb[2], :]
             scaled = cv2.resize(cropped, (face_crop_size, face_crop_size), interpolation=cv2.INTER_AREA)
-            if do_prewhiten:
+            if normalize == NORMALIZE_IMAGE:
                 images.append(prewhiten(scaled))
+            elif normalize == NORMALIZE_FIXED:
+                images.append(fixed_normalize(scaled))
             else:
                 images.append(scaled)
 