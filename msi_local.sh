f95eabdda68949ec45b37e01fdcde5a7f7ceccac
Alexander Gunin <guninalexander@gmail.com>
2019-08-09 13:09:35 -0700

    add start option

---
+++ msi_local.sh
@@ -0,0 +1,14 @@
+source activate svod
+source /opt/intel/openvino/bin/setupvars.sh -pyver 3.6
+
+./streaming.sh \
+    --token '469d7155-c74f-4c88-aed9-e5ca9c8b3411' \
+    --input server \
+    --output mjpg \
+    --base-url https://dev.kibernetika.io/api/v0.2 \
+    --model-name ponchik \
+    --workspace-name expo-recall \
+    --inference-fps 0 \
+    --head-pose-path models/head_pose/head-pose-estimation-adas-0001.xml \
+    --slack-token '' \
+    --slack-channel ''
