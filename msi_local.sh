545c0895246fa772950dbcbe705fe1a0740fd1fc
Alexander Gunin <guninalexander@gmail.com>
2019-08-09 13:28:08 -0700

    use realsense by default

--- msi_local.sh
+++ msi_local.sh
@@ -3,8 +3,8 @@ source /opt/intel/openvino/bin/setupvars.sh -pyver 3.6
 
 ./streaming.sh \
     --token '469d7155-c74f-4c88-aed9-e5ca9c8b3411' \
-    --input server \
-    --output mjpg \
+    --input realsense \
+    --output hls \
     --base-url https://dev.kibernetika.io/api/v0.2 \
     --model-name ponchik \
     --workspace-name expo-recall \
