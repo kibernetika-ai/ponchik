706d23b379172436fa7e5d4cb6009af0492d33a1
Alexander Gunin <guninalexander@gmail.com>
2019-08-09 13:45:27 -0700

    reduce fps

--- msi_local.sh
+++ msi_local.sh
@@ -1,4 +1,5 @@
 source activate svod
+
 source /opt/intel/openvino/bin/setupvars.sh -pyver 3.6
 
 ./streaming.sh \
@@ -8,7 +9,7 @@ source /opt/intel/openvino/bin/setupvars.sh -pyver 3.6
     --base-url https://dev.kibernetika.io/api/v0.2 \
     --model-name ponchik \
     --workspace-name expo-recall \
-    --inference-fps 0 \
+    --inference-fps 2 \
     --head-pose-path models/head_pose/head-pose-estimation-adas-0001.xml \
     --slack-token '' \
     --slack-channel ''
