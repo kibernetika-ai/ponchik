cf8ae3442f44d77f53e81637bdd8331430b6defb
Alex <agunin@kuberlab.com>
2019-08-11 22:22:41 -0700

    script changes

--- msi_local.sh
+++ msi_local.sh
@@ -9,7 +9,7 @@ source /opt/intel/openvino/bin/setupvars.sh -pyver 3.6
     --base-url https://dev.kibernetika.io/api/v0.2 \
     --model-name ponchik \
     --workspace-name expo-recall \
-    --inference-fps 2 \
+    --inference-fps 0 \
     --head-pose-path models/head_pose/head-pose-estimation-adas-0001.xml \
-    --slack-token '' \
-    --slack-channel ''
+    --slack-token 'xoxb-104043499155-617859204675-14SVmMGhq52pPJLXKmBTZYoA' \
+    --slack-channel 'svod-faces'
