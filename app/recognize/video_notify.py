b75ffb33c0ec5eb21909415e2e16bf1384fe5053
Alexander Gunin <guninalexander@gmail.com>
2019-08-13 22:21:06 -0700

    foce notify

--- app/recognize/video_notify.py
+++ app/recognize/video_notify.py
@@ -41,8 +41,8 @@ class InVideoDetected:
             self.counter += c
             now = time()
             if (now - self.last) > self.notify_period:
-                if self.counter > 3:
-                    if self.dists / self.counter < 0.4:
+                if self.counter > 4:
+                    if self.dists / self.counter < 0.39:
                         self.notified = True
                         self.notified_awaiting = True
                         self.dists = 0
