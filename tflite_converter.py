d23d6465b81bc86c8ae944531a2e5921b3489c3a
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2019-08-20 17:03:04 +0400

    Try tflite converter

---
+++ tflite_converter.py
@@ -0,0 +1,71 @@
+import argparse
+import glob
+import os
+import itertools
+
+import cv2
+import tensorflow as tf
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--graph',
+    )
+    parser.add_argument(
+        '--saved-model'
+    )
+    parser.add_argument(
+        '--data-dir'
+    )
+    parser.add_argument(
+        '--limit-steps',
+        type=int,
+        default=0,
+    )
+    return parser.parse_args()
+
+
+def normalize(img):
+    normalized = img / 127.5 - 1.0
+    return normalized
+
+
+def main():
+    args = parse_args()
+
+    def representative_dataset_gen(dataset_dir):
+        dataset_dir = dataset_dir.rstrip('/')
+        jpg_iter = glob.iglob(dataset_dir + '/**/*.jpg')
+        jpeg_iter = glob.iglob(dataset_dir + '/**/*.jpeg')
+        png_iter = glob.iglob(dataset_dir + '/**/*.png')
+        iterator = itertools.chain(jpg_iter, jpeg_iter, png_iter)
+        i = 0
+        for path in iterator:
+            img = cv2.imread(path, cv2.IMREAD_COLOR)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            i += 1
+            if args.limit_steps != 0 and i >= args.limit_steps:
+                break
+
+            yield normalize(img)
+
+    if args.graph:
+        converter = tf.lite.TFLiteConverter.from_frozen_graph(
+            args.graph,
+            input_arrays=['input'],
+            output_arrays=['embeddings'],
+            input_shapes={'input': [1, 160, 160, 3]}
+        )
+    else:
+        converter = tf.lite.TFLiteConverter.from_saved_model(args.saved_model)
+
+    # converter.optimizations = [tf.lite.]
+    converter.representative_dataset = representative_dataset_gen
+    tflite_quant_model = converter.convert()
+
+    # __import__('ipdb').set_trace()
+
+
+if __name__ == '__main__':
+    main()
